d9a0e84b86785e37b0e8120ea44046f107840895
Patrik <bagenholm.patrik@hotmail.com>
2019-03-06 15:18:34 +0100

    Add ID, fix crash

--- src/main/java/org/ia/reviewperbuck/Axesor.java
+++ src/main/java/org/ia/reviewperbuck/Axesor.java
@@ -3,12 +3,16 @@ package org.ia.reviewperbuck;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.HashMap;
 
 @Data
 @Entity
 public class Axesor {
 
+
+    private @Id @GeneratedValue long id;
     private String model;
     private float rating;
     private HashMap<String, String> specs;
