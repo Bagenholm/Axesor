00adfd0c1c2f748975e313ec7dfdcaa06214aa38
AdamBremholm <skiryoland@gmail.com>
2019-03-07 14:58:27 +0100

    Create getToken method.

--- src/main/java/org/ia/reviewperbuck/AxesorController.java
+++ src/main/java/org/ia/reviewperbuck/AxesorController.java
@@ -1,15 +1,20 @@
 package org.ia.reviewperbuck;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.http.*;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.StringReader;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -20,6 +25,9 @@ public class AxesorController {
 
     List<Television> televisions;
     List<Review> reviews;
+
+    String accessToken;
+
 
     public AxesorController(RestTemplate restTemplate, Storage storage) {
         this.storage = storage;
@@ -76,4 +84,34 @@ public class AxesorController {
     public List<Axesor> getAll() {
         return storage.findAll();
     }
+
+
+    @PostMapping("/getToken")
+    public String getToken(RequestEntity<String> requestEntity) {
+
+        System.out.println(requestEntity.getBody());
+
+        String rawToken = requestEntity.getBody();
+
+        accessToken = rawToken.substring(1, rawToken.length()-1);
+
+        return accessToken;
+
+    }
+    @GetMapping("/getHeader")
+        public ResponseEntity<Map<String, Object>> getHeader(HttpServletRequest request ) {
+
+
+        System.out.println(request.getHeader("Authorization"));
+        Map<String, Object> returnValue = new HashMap<>();
+        Enumeration<String> hearderNames = request.getHeaderNames();
+        while(hearderNames.hasMoreElements())
+        {
+            String headerName = hearderNames.nextElement();
+            returnValue.put(headerName, request.getHeader(headerName));
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(returnValue);
+    }
+
 }
